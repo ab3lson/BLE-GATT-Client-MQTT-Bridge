20cad6a81cd505d97ef135246e857f334ba8cf87
Chase <ablec@byu.edu>
2021-02-03 05:39:26 +0000

    folder restructure and added connection retry

--- config/sensors_v2.py
+++ config/sensors_v2.py
@@ -1,3 +1,9 @@
-24:62:AB:FF:49:46 beb5483e-36e1-4688-b7f5-ea07361b26a8 char
-34:14:B5:92:49:B2 0000dfb1-0000-1000-8000-00805f9b34fb notify
+# Follow the format below for each device, with a space separating each field:
+# <MAC ADDRESS> <CHARACTERISTIC_UUID> <char|notify>
+# Select char if the value is written directly to the characteristic
+# Select notify if the value must be sent "SEND" to start broadcasting data
+#
+#
+#24:62:AB:FF:49:46 beb5483e-36e1-4688-b7f5-ea07361b26a8 char
+#34:14:B5:92:49:B2 0000dfb1-0000-1000-8000-00805f9b34fb notify
 64:69:4E:80:20:17 0000ffe1-0000-1000-8000-00805f9b34fb notify
---
+++ get_data.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+import pygatt
+import time
+import json
+import paho.mqtt.client as mqtt
+import config.creds as creds
+
+#BLE VARIABLES
+adapter = pygatt.backends.GATTToolBackend()
+sensors = []
+values = []
+notification = ""
+
+#MQTT VARIABLES
+BROKER = '192.168.0.34'
+MQTT = mqtt.Client()
+MQTT.username_pw_set(username=creds.USERNAME,password=creds.PASSWORD)
+MQTT.connect(BROKER)
+
+def handle_data(handle, value):
+    """
+    handle -- integer, characteristic read handle the data was received on
+    value -- bytearray, the data returned in the notification
+    """
+    global notification
+    notification = bytes(value).decode('utf-8')
+
+#GET SENSOR INFO
+print("Getting sensor information...")
+sensor_file = open('./config/sensors_v2.py', 'r')
+sensor_info = sensor_file.readlines()
+for line in sensor_info:
+    if ' ' in line and '#' not in line:
+        try:
+            split = line.strip().split(' ')
+            if (split[2] != None): sensors.append({'device': split[0], 'characteristic': split[1], 'notif': True})
+            else: sensors.append({'device': split[0], 'characteristic': split[1], 'notif': False})
+        except:
+            print(f"[ERROR] Bad sensor formatting: {line.strip()}\nTry formatting like: MAC_ADDRESS CHAR_UUID [notif/char]")
+
+
+print(f"Getting data from {len(sensors)} sensors!")
+for sensor in sensors:
+    connection_attempts = 0
+    while connection_attempts < 3:
+        print(f"- Connection attempt #{connection_attempts}")
+        try:
+            adapter.start()
+            if not sensor["notif"]: # Reads the characteristic data directly, if supported
+                print(f"[INFO] Connecting to device: {sensor['device']}")
+                device = adapter.connect(sensor['device'])
+                print(f"[INFO] Reading characteristic: {sensor['characteristic']}")
+                value = bytes(device.char_read(sensor['characteristic'])).decode('utf-8')
+                values.append({'device': sensor['device'], 'value': value})
+            else:	# Sends "SEND" to the device and waits for a notification to respond
+                print(f"[INFO] Connecting to device: {sensor['device']}")
+                device = adapter.connect(sensor['device'])
+                device.char_write(sensor['characteristic'], bytearray([0x53, 0x45, 0x4e, 0x44])) # Sends "SEND" to the device
+                try:
+                    time.sleep(2)
+                    print("Subscribing...")
+                    device.subscribe(sensor['characteristic'], callback=handle_data)
+                    time.sleep(2)
+                except Exception as e:
+                    if (e != None or e != "None"): print("Exception:",e)
+                print("Stopping Adapter...")
+                adapter.stop()
+                values.append({'device': sensor['device'], 'value': notification})
+                notification = ""
+                connection_attempts = 4 # Stops trying to connect, because it succeeded
+        except Exception as e:
+            print(f"[ERROR] Could not connect to {sensor['device']}")
+        finally:
+            adapter.stop()
+            connection_attempts += 1
+
+
+
+
+#print("Retrieved values:", values)
+
+MQTT.publish("ble/test", json.dumps(values))
+time.sleep(1)
+
+MQTT.disconnect()
